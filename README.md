f5b7c2b3b775ea7e568f07faea12bd6ca33086c4
mkheck13 <mkheck13@gmail.com>
2024-12-09 15:52:47 -0800

    update in readme

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
+# Weather Sprint
 
+![Design preview for the weather app]()
+
+**Description:** Build out a desktop application from the provided figma.
+
+**Developer:** Michael Heckerman
+
+**Date Revised:** 12/9/24
 
 
 [Figma](https://www.figma.com/design/ldUWQTiHsWodPq4GH4jeok/Weather-App?node-id=0-1&t=YdltH3a8wa8KZsqC-1)
 
-## Day 1
-Working on getting the data to display for the current location. I have gotten the current temp, min and max.
-I also have the description to display along with the current city. I still need to work on getting the state and country to display with the city. Also need the icon to show. And ability to search. Styling is needed, but only after I can get all the data to display properly.
+## End Of Day Update
+
+**Day 1**
+
+Completed:
+- Working on getting the data to display for the current location. 
+- I have gotten the current temp, min and max.
+- I also have the description to display along with the current city with the state and country. 
+
+Working On:
+- Also need the icon to show. 
+- And ability to search. 
+- Styling is needed, but only after I can get all the data to display properly.
--- scripts/app.js
+++ scripts/app.js
@@ -10,23 +10,42 @@ let mainMaxTemp = document.getElementById("mainMaxTemp");
 
 let currentWeatherData;
 let locationData;
+let userSearch;
+let userLat;
+let userLong;
 
-// navigator.geolocation.getCurrentPosition(success, errorFunc);
+navigator.geolocation.getCurrentPosition(success, errorFunc);
 
-// function apiCall () {
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
-//     .then((response) => {
-//         return response.json()
-//     })
-//     .then((data) => {
-//         console.log(data);
-//     })
-// }
+async function success(position) {
+    if(userSearch.value){
+        // const promise = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userSearch.value}&limit=5&appid=${APIKEY}`);
+        const data = await promise.json();
+        userLat = data[0].lat;
+        userLong = data[0].lon;
+    }
+    else{
+        userLat = position.coords.latitude;
+        userLong = position.coords.longitude;
+    }
+    await currentWeatherAPI();
+    await getLocationAPI();
+    
+}
 
-// apiCall();
+async function errorFunc(error) {
+    console.log(error.message);
+    userLat = 37.9577;
+    userLong = -121.2908;
 
+    await currentWeatherAPI();
+    await getLocationAPI();
+    
+}
+
+
+// function to get the temp for the current location
 async function currentWeatherAPI(){
-    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}&units=imperial`);
+    // const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}&units=imperial`);
     const data = await promise.json();
     currentWeatherData = data;
 
@@ -41,13 +60,14 @@ async function currentWeatherAPI(){
 }
 currentWeatherAPI();
 
+// function to get and display the current location with state and country
 async function getLocationAPI() {
-    const location = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=37.9577&lon=-121.2908&limit=5&appid=${APIKEY}`);
+    // const location = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=37.9577&lon=-121.2908&limit=5&appid=${APIKEY}`);
     const data = await location.json();
     locationData = data;
 
     // location
-    mainLocation.innerHTML = locationData[0].name.toUpperCase();
+    mainLocation.innerHTML = `${locationData[0].name.toUpperCase()}, ${locationData[0].state.toUpperCase()}, ${locationData[0].country.toUpperCase()}`;
     
 }
 getLocationAPI();
